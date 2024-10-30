7f898ab9986db93f2f2e17b8078e8844162aa884
Carlos Vieira <carlos.vieira@digitalcollegesul.local>
2024-10-30 10:04:29 -0300

    feat: change to dotenv

---
+++ models/address.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Address extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Address.belongsTo(models.User, { foreignKey: 'id' })
+    }
+  }
+  Address.init({
+    userId: DataTypes.INTEGER,
+    street: DataTypes.STRING,
+    city: DataTypes.STRING,
+    state: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Address',
+  });
+  return Address;
+};
